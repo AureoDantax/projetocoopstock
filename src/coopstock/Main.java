043880a93b85f580394e05a778c44305d18db74f
Aureo Dantas <dantasaureo19@gmail.com>
2023-10-31 03:39:27 -0300

    Removendo imports não usados, identação,removendo redundância

--- src/coopstock/Main.java
+++ src/coopstock/Main.java
@@ -1,7 +1,6 @@
 package coopstock;
 
 
-import java.util.Objects;
 import java.util.Scanner;
 
 import static servico.ImplementaServico.cadastraProduto;
@@ -11,40 +10,40 @@ public class Main {
     public static void main(String[] args) {
 
 
-
         menu();
     }
 
     public static void menu() {
-        String op = "";
+        String op;
         Scanner input = new Scanner(System.in);
 
         do {
-            System.out.println("\n\nCoopStock, solução inteligente em estoque" +
-                    "\n\n\nBem vindo!\n\nO que deseja realizar hoje?");
+            System.out.println("""
+
+
+                    CoopStock, solução inteligente em estoque
+
+
+                    Bem vindo!
+
+                    O que deseja realizar hoje?""");
             System.out.println("""
                     1- CADASTRAR PRODUTO
                     2- CONSULTAR PRODUTO
                     3- ENCERRAR""");
             op = input.next();
-            if(!op.equals("1") && !op.equals("2") && op.equals("3")){
+            if (!op.equals("1") && !op.equals("2") && op.equals("3")) {
                 System.out.println("Opção inválida, digite uma opção do menu em forma numeral!");
 
             }
 
             switch (op) {
-                case "1": {
-                cadastraProduto();
-                    break;
-                }
-                case "2": {
-                consultarProdutos();
-                break;
-                }
-                case "3": {
-                    System.out.println("Até mais!");
-                    break;
-                }
+                case "1" -> cadastraProduto();
+
+                case "2" -> consultarProdutos();
+
+                case "3" -> System.out.println("Até mais!");
+
             }
 
         } while (!op.equals("3"));
